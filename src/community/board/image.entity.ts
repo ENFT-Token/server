4467df3c01fe8a8d90d9c0371d879633a497ed1b
baehaejin <bae05030@gmail.com>
2022-05-12 00:22:21 +0900

    modify image type : string to blob

--- src/community/board/image.entity.ts
+++ src/community/board/image.entity.ts
@@ -1,13 +1,13 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Board } from './board.entity';
-
+import {Blob} from 'buffer';
 @Entity('image')
 export class Image {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  image: string;
+  @Column("blob", { nullable: true })
+  image: Blob;
 
   @ManyToOne((type) => Board, (board) => board.image, { eager: false })
   board: Board;
