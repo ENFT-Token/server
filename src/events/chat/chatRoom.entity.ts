fc239bb35b97e5525ee92cd87bcf811c1fceb72d
baehaejin <bae05030@gmail.com>
2022-06-07 15:37:57 +0900

    modify chat_room databasel

--- src/events/chat/chatRoom.entity.ts
+++ src/events/chat/chatRoom.entity.ts
@@ -6,7 +6,7 @@ export class ChatRoom {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({unique: true})
   roomId: string;
 
   @OneToMany((type) => Chat, (chat) => chat.roomId, { eager: true })
